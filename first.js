2a18dbaf8da49fbb235e60fd0047ec1936627771
radudeacu <deacuradu@gmail.com>
2024-11-05 18:45:53 +0200

    two sum problem variants

---
+++ first.js
@@ -0,0 +1,16 @@
+
+
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return null;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(twoSum([3, 2, 4], 6));      // Output: [1, 2]
+console.log(twoSum([3, 3], 6));         // Output: [0, 1]
---
+++ second.js
@@ -0,0 +1,18 @@
+
+
+function twoSumOptimized(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return null;
+}
+
+console.log(twoSumOptimized([2, 7, 11, 15], 9)); // Output: [0, 1]
+console.log(twoSumOptimized([3, 2, 4], 6));      // Output: [1, 2]
+console.log(twoSumOptimized([3, 3], 6));         // Output: [0, 1]
